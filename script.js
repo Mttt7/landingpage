9acf37caaa6b1cd06b42162c4ade98119125d8ff
mttt <groobytt@gmail.com>
2022-09-29 20:06:18 +0200

    repair smooth scrolling by clicking >my projects<

---
+++ script.js
@@ -0,0 +1,25 @@
+const ig=document.querySelector("#ig")
+const github=document.querySelector("#github")
+
+const calculator=document.querySelector("#calculator")
+const sketchbook=document.querySelector("#sketchbook")
+const rps=document.querySelector("#rps")
+
+ig.onclick = ()=>{
+    window.open("https://www.instagram.com/mtt.jpeg/")
+}
+github.onclick = ()=>{
+    window.open("https://github.com/Mttt7")
+}
+
+calculator.onclick = ()=>{
+    window.open("https://mttt7.github.io/calculator/")
+}
+sketchbook.onclick = ()=>{
+    window.open("https://mttt7.github.io/Etch-a-Sketch/")
+}
+rps.onclick = ()=>{
+    window.open("https://mttt7.github.io/Rock-Paper_Scissors/")
+}
+
+
