75b4d9ee645b60fec376599af28b34285772e31a
mttt <groobytt@gmail.com>
2022-11-09 11:14:16 +0100

    add Library project and change links to a tag instead jscript

--- script.js
+++ script.js
@@ -7,29 +7,10 @@ const rps=document.querySelector("#rps")
 const isprime = document.querySelector("#isprime")
 const admin = document.querySelector("#admin")
 
-ig.onclick = ()=>{
-    window.open("https://www.instagram.com/mtt.jpeg/")
-}
-github.onclick = ()=>{
-    window.open("https://github.com/Mttt7")
-}
 
-calculator.onclick = ()=>{
-    window.open("https://mttt7.github.io/calculator/")
-}
-isprime.onclick=() =>{
-    window.open("https://mttt7.github.io/prime-numbers")
-}
 
-sketchbook.onclick = ()=>{
-    window.open("https://mttt7.github.io/Etch-a-Sketch/")
-}
-rps.onclick = ()=>{
-    window.open("https://mttt7.github.io/Rock-Paper_Scissors/")
-}
 
-admin.onclick = ()=>{
-    window.open("https://mttt7.github.io/admin-dashboard/")
-}
+
+
 
 
